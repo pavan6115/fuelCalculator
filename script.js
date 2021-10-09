20244643add7901e993b0cc6f80e3dc2871f14c6
kpavann <kulkarnipavan6115@gmail.com>
2021-10-09 19:39:24 +0530

    feat: logic done

---
+++ script.js
@@ -0,0 +1,33 @@
+const vehicleAvg = document.querySelector('#vehicle-avg')
+const totalDistance = document.querySelector('#total-dist')
+const totalPersons = document.querySelector('#total-persons')
+const fuelPerLitre = document.querySelector('#fuel-per-litre')
+const checkControBtn = document.querySelector('.checkContro-btn')
+const totalFuelMsg = document.querySelector('.totalFuel-msg')
+const controMoneyMsg = document.querySelector('.controMoney-msg')
+
+
+checkControBtn.addEventListener('click', checkContribution)
+
+
+function checkContribution(){
+    if (vehicleAvg.value < 0 || vehicleAvg.value === "" ||
+    totalDistance.value < 0 || totalDistance.value === "" || totalPersons.value < 0 || totalPersons.value === "" || fuelPerLitre.value < 0 || fuelPerLitre.value === ""){
+        outputMsg.innerText = "Check all values"
+    }
+    else {
+        const fuelRequiredInLitres = totalDistance.value/vehicleAvg.value;
+
+        totalFuelMsg.innerText = `Total Fuel Required in Litres : ${fuelRequiredInLitres.toFixed(2)} L`
+
+        const costForFuel = fuelPerLitre.value * fuelRequiredInLitres;
+
+        // console.log('total cost of fuel - ',costForFuel)
+
+        const perPersonContro = costForFuel / totalPersons.value;
+
+        // console.log('Per person contro - ',perPersonContro)
+
+        controMoneyMsg.innerText = `Each person contro will be ${perPersonContro.toFixed(3)}/-`
+    }
+}
